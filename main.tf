b55ce85c36829711d29b10cc269d253ac9bbd69a
Mehdi Bernoussi <mehdiber@amazon.com>
2023-04-05 01:14:08 +0000

    add bucket

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ terraform {
   }
 }
 
+data "aws_caller_identity" "current" {}
+
 data "aws_vpc" "selected" {
   id = var.vpc_id
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "tf-test-bucket-${data.aws_caller_identity.current.account_id}"
 }
