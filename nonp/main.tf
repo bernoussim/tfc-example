69a60ea4687b93606a9786fc4c3246c92dce1c66
Mehdi Bernoussi <mehdiber@amazon.com>
2023-04-10 21:24:43 +0000

    add secret

--- nonp/main.tf
+++ nonp/main.tf
@@ -17,3 +17,7 @@ module "tfc_workspace" {
 resource "aws_s3_bucket" "b" {
   bucket = "tf-test-bucket-${data.aws_caller_identity.current.account_id}-${module.tfc_workspace.vpc_id}"
 }
+
+data "aws_secretsmanager_secret" "test" {
+  name = "test"
+}
