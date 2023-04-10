a359248c898cd2584ba3da612a563960ba86a96a
Mehdi Bernoussi <mehdiber@amazon.com>
2023-04-10 02:56:45 +0000

    add token

--- dev/ws1/ws1.tf
+++ dev/ws1/ws1.tf
@@ -1,5 +1,6 @@
 provider "tfe" {
   hostname = "app.terraform.io"
+  token    = "Cl9OSvegbn2Rzg.atlasv1.yrUTyWIUyxIgTEpOZY0X8FIzTxCJL8HWdZB4DpabP4SXANdCxSprH86QNqWhOacJYEU"
 }
 
 data "tfe_workspace" "dev_ws1" {
@@ -7,11 +8,11 @@ data "tfe_workspace" "dev_ws1" {
   organization = "mehdiber"
 }
 
-#resource "tfe_variable" "vpc_name" {
-#  key          = "vpc_id"
-#  value        = "vpc-0ebbc4ffbff8c9689"
-#  category     = "terraform"
-#  workspace_id = data.tfe_workspace.dev_ws1.id
-#  description  = "vpc id"
-#}
+resource "tfe_variable" "vpc_name" {
+  key          = "vpc_id"
+  value        = "vpc-0ebbc4ffbff8c9689"
+  category     = "terraform"
+  workspace_id = data.tfe_workspace.dev_ws1.id
+  description  = "vpc id"
+}
 
