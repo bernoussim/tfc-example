f5ba7fba729999d6e7a748138d488f85d7423df5
Mehdi Bernoussi <mehdiber@amazon.com>
2023-04-10 01:21:03 +0000

    refacto

--- dev/ws1/ws1.tf
+++ dev/ws1/ws1.tf
@@ -1,19 +1,13 @@
-resource "tfe_workspace" "dev_ws1" {
-  name             = "dev-ws1"
-  organization     = "mehdiber"
-  trigger_patterns = ["/dev/ws1/*.tf"]
-  vcs_repo {
-    identifier                 = "bernoussim/tfc-example"
-    branch                     = "master"
-    github_app_installation_id = "ghain-bdeYvwZxSPuaXaKM"
-  }
+data "tfe_workspace" "dev_ws1" {
+  name         = "dev-ws1"
+  organization = "mehdiber"
 }
 
 resource "tfe_variable" "vpc_name" {
   key          = "vpc_id"
   value        = "vpc-0ebbc4ffbff8c9689"
   category     = "terraform"
-  workspace_id = tfe_workspace.dev_ws1.id
+  workspace_id = data.tfe_workspace.dev_ws1.id
   description  = "vpc id"
 }
 
@@ -22,11 +16,11 @@ resource "tfe_variable" "region" {
   value        = "us-east-1"
   category     = "env"
   description  = "aws region"
-  workspace_id = tfe_workspace.dev_ws1.id
+  workspace_id = data.tfe_workspace.dev_ws1.id
 }
 
 resource "tfe_variable" "enable_aws_provider_auth" {
-  workspace_id = tfe_workspace.dev_ws1.id
+  workspace_id = data.tfe_workspace.dev_ws1.id
 
   key      = "TFC_AWS_PROVIDER_AUTH"
   value    = "true"
@@ -36,7 +30,7 @@ resource "tfe_variable" "enable_aws_provider_auth" {
 }
 
 resource "tfe_variable" "tfc_aws_role_arn" {
-  workspace_id = tfe_workspace.dev_ws1.id
+  workspace_id = data.tfe_workspace.dev_ws1.id
 
   key      = "TFC_AWS_RUN_ROLE_ARN"
   value    = "arn:aws:iam::082028366202:role/tfc-role"
