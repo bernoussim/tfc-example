9e0b4042d2a11d2bc4718a705cf101510fa9126f
Mehdi Bernoussi <mehdiber@amazon.com>
2023-04-10 01:34:13 +0000

    add variables

--- dev/ws1/ws1.tf
+++ dev/ws1/ws1.tf
@@ -11,30 +11,3 @@ resource "tfe_variable" "vpc_name" {
   description  = "vpc id"
 }
 
-resource "tfe_variable" "region" {
-  key          = "AWS_REGION"
-  value        = "us-east-1"
-  category     = "env"
-  description  = "aws region"
-  workspace_id = data.tfe_workspace.dev_ws1.id
-}
-
-resource "tfe_variable" "enable_aws_provider_auth" {
-  workspace_id = data.tfe_workspace.dev_ws1.id
-
-  key      = "TFC_AWS_PROVIDER_AUTH"
-  value    = "true"
-  category = "env"
-
-  description = "Enable the Workload Identity integration for AWS."
-}
-
-resource "tfe_variable" "tfc_aws_role_arn" {
-  workspace_id = data.tfe_workspace.dev_ws1.id
-
-  key      = "TFC_AWS_RUN_ROLE_ARN"
-  value    = "arn:aws:iam::082028366202:role/tfc-role"
-  category = "env"
-
-  description = "The AWS role arn runs will use to authenticate."
-}
